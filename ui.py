78273931b26b792a5ee30b72b19634fbd1d62698
Javier Moreno <javiermorenoprieto@gmail.com>
2024-08-07 17:31:12 +0200

    Switch between fullscreen and window and Ready when idle

--- ui.py
+++ ui.py
@@ -4,20 +4,27 @@ import multiprocessing
 def update_display(queue):
     pygame.init()
     print(pygame.font.get_fonts())
-    # screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)  # Set the display mode to fullscreen
-    screen = pygame.display.set_mode((1080, 720), pygame.RESIZABLE)
-    pygame.display.set_caption('Buzzers')
+    largest_size = pygame.display.list_modes()[0]
+    screen = pygame.display.set_mode(largest_size, pygame.FULLSCREEN)  # Set the display mode to fullscreen
     big_font = pygame.font.Font("data/fonts/SuperMario256.ttf", 104)
     small_font = pygame.font.Font("data/fonts/SuperMario256.ttf", 54)
     team_text = ""
     time_text = ""
     team_color = (255, 255, 255)
+    fullscreen = True  # Flag to track the current display mode
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE and fullscreen:
+                    screen = pygame.display.set_mode((1080, 720), pygame.RESIZABLE)
+                    fullscreen = False
+                elif event.key == pygame.K_F11 and not fullscreen:
+                    screen = pygame.display.set_mode(largest_size, pygame.FULLSCREEN)
+                    fullscreen = True
 
         try:
             message = queue.get_nowait()
@@ -39,6 +46,11 @@ def update_display(queue):
             rendered_time_text = small_font.render(time_text, True, (255, 255, 255))
             time_text_rect = rendered_time_text.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2 + 100))
             screen.blit(rendered_time_text, time_text_rect)
+        if not team_text and not time_text:
+            rendered_text = big_font.render("Ready", True, (255,255,255))
+            text_rect = rendered_text.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2))
+            screen.blit(rendered_text, text_rect)
+
         pygame.display.flip()
         pygame.time.wait(100)
 
